1e856a89cbd58dd5d1358a87d9864c9136bbf7df
Arthur Manenti <arthur.manenti.videira@gmail.com>
2026-06-06 19:05:15 -0300

    Instalando axios e criando comando para foto de gato

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const axios = require("axios");
 require("dotenv").config();
 
 const { App } = require("@slack/bolt");
@@ -21,6 +22,30 @@ app.command("/manenti-help", async ({ ack, respond }) => {
 /manenti-hw - Display "Hello, World!"
 /manenti-help - Show available commands`,
   });
+});
+
+app.command("/manenti-cat", async ({ ack, respond }) => {
+  await ack();
+  try {
+    const response = await axios.get(
+      "https://api.thecatapi.com/v1/images/search",
+    );
+    const imageUrl = response.data[0].url;
+
+    await respond({
+      blocks: [
+        {
+          type: "image",
+          image_url: imageUrl,
+          alt_text: "Image of a cat",
+        },
+      ],
+    });
+  } catch (error) {
+    await respond({
+      text: `Sorry, I couldn't fetch a cat image at the moment.\n error: ${error.message}`,
+    });
+  }
 });
 
 (async () => {
