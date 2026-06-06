f8b4edbc18036c9e6a16778426e379aebc0c7e64
Arthur Manenti <arthur.manenti.videira@gmail.com>
2026-06-06 19:35:06 -0300

    Adicionando comando de pokemon

--- index.js
+++ index.js
@@ -35,6 +35,13 @@ app.command("/manenti-cat", async ({ ack, respond }) => {
     await respond({
       blocks: [
         {
+          type: "section",
+          text: {
+            type: "mrkdwn",
+            text: "*Here's a cat!*",
+          },
+        },
+        {
           type: "image",
           image_url: imageUrl,
           alt_text: "Image of a cat",
@@ -44,6 +51,49 @@ app.command("/manenti-cat", async ({ ack, respond }) => {
   } catch (error) {
     await respond({
       text: `Sorry, I couldn't fetch a cat image at the moment.\n error: ${error.message}`,
+    });
+  }
+});
+
+app.command("/manenti-pokemon", async ({ command, ack, respond }) => {
+  await ack();
+  try {
+    const response = await axios.get(
+      `https://pokeapi.co/api/v2/pokemon/${command.text}`,
+    );
+    const imageUrl = response.data.sprites.front_default;
+    const pokemonName = response.data.name;
+    const type = response.data.types[0].type.name;
+    const abilities = response.data.abilities
+      .map((ability) => ability.ability.name)
+      .join(", ");
+    const stats = response.data.stats
+      .map((stat) => `${stat.stat.name}: ${stat.base_stat}`)
+      .join("\n");
+
+    await respond({
+      blocks: [
+        {
+          type: "section",
+          text: {
+            type: "mrkdwn",
+            text: `**Here's a ${pokemonName}!**
+            *Type*: ${type}
+            *Abilities*: ${abilities}
+            *Stats*: \n${stats}
+            `,
+          },
+        },
+        {
+          type: "image",
+          image_url: imageUrl,
+          alt_text: `Image of ${pokemonName}`,
+        },
+      ],
+    });
+  } catch (error) {
+    await respond({
+      text: `Sorry, I couldn't fetch a pokemon at the moment.\n error: ${error.message}`,
     });
   }
 });
